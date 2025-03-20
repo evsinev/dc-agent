579dd4a5c4e7b745b5f56d8f9ffaa9882a15d4db
esinev <es@payneteasy.com>
2025-03-20 14:57:10 +0300

    [dc-agent] Remove old files on node task

--- dc-agent-app/src/main/java/com/payneteasy/dcagent/servlets/NodeServlet.java
+++ dc-agent-app/src/main/java/com/payneteasy/dcagent/servlets/NodeServlet.java
@@ -5,6 +5,7 @@ import com.payneteasy.dcagent.core.config.service.IConfigService;
 import com.payneteasy.dcagent.core.modules.jar.DaemontoolsServiceImpl;
 import com.payneteasy.dcagent.core.modules.jar.ILog;
 import com.payneteasy.dcagent.core.modules.zipachive.ZipFileExtractor;
+import com.payneteasy.dcagent.core.util.DeleteDirRecursively;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,8 +21,14 @@ public class NodeServlet extends AbstractJarServlet {
 
     @Override
     protected void postProcessJarFile(ILog log, File aJarFile) {
+        File targetDir = aJarFile.getParentFile();
+
+        DeleteDirRecursively deleteDirRecursively = new DeleteDirRecursively(targetDir);
+        deleteDirRecursively.deleteDirIfExists(new File(targetDir, "app"));
+        deleteDirRecursively.deleteDirIfExists(new File(targetDir, "target"));
+
         try {
-            zipExtractor.extractZip(aJarFile, aJarFile.getParentFile());
+            zipExtractor.extractZip(aJarFile, targetDir);
         } catch (IOException e) {
             throw new IllegalStateException("Cannot extract zip file " + aJarFile.getAbsolutePath(), e);
         }
